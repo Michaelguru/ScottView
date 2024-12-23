9e069aa7abaaf47f3a27c6b853c71f28408faf73
Michaelguru <161510080+Michaelguru@users.noreply.github.com>
2024-12-23 14:18:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,30 @@ document.addEventListener("DOMContentLoaded", function(){
         observer.observe(section);
     });
 });
+
+
+
+// Select the toggle button
+const themeToggle = document.getElementById('theme-toggle');
+
+// Check and apply saved theme from localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+}
+
+// Add event listener to toggle theme
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+
+  // Save the theme preference to localStorage
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+    themeToggle.textContent= "Light Mode";
+  } else {
+    localStorage.setItem('theme', 'light');
+    themeToggle.textContent= "Dark Mode";
+    
+  }
+});
